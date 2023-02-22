5d96b7863817b77e03eb42d472a57cb742dfb178
davidevigano166 <dadaviga@gmail.com>
2023-02-22 19:36:35 +0100

    New Updates

--- script.js
+++ script.js
@@ -7,4 +7,10 @@ window.onload = function() {
 }
 
 $(document).ready(function(){
+    for (i=0; i<9; i++) {
+        x = Math.random() * (90 - 0)
+        y = Math.random() * (90 - 0)
+        $('.background-img:nth-child('+i+')').css('top', y + 'vh')
+        $('.background-img:nth-child('+i+')').css('left', x + 'vw')
+    }
 });
